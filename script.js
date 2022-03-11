f85a05adcbab1a29ac7788bdd172d1c73f7281d1
Daniel Wilson <dww6@students.waikato.ac.nz>
2022-03-11 23:35:32 +1300

    Implemented JS code to show the header section a user is at

--- script.js
+++ script.js
@@ -41,3 +41,44 @@ function transition() {
         time = time + 100;
     }
 }
+
+//Gets each of the navigation links
+const navTags = document.querySelectorAll(".site-nav");
+//Gets each of the segments which the navigation links direct to
+const segments = document.querySelectorAll(".segment");
+//Stores the ID of the closest segment that a user is viewing
+let closestSegment = "";
+
+//Checks the users position on the page every time a scroll event occurs
+window.onscroll = () => {
+
+    //Goes through and checks each of the segments present on the webpage
+    //to find the closest segment (Furthermost down the page )
+    segments.forEach((segment) => {
+        //Works out the position of the segment on the page and subtracts 70 pixels
+        //to give extra whitespace to account for the height of the header.
+        const segmentTop = segment.offsetTop - 70;
+        //Checks if the user is scrolled to a point where a segment is past the
+        //current position of the header bar by getting the current Y Offset
+        //(How many pixels down the page)
+        if (pageYOffset >= segmentTop) {
+            //Assigns the ID of this segment to be the current closestSegment
+            closestSegment = segment.getAttribute("id"); 
+        }
+    });
+
+    //Goes through each of the navigation list items in order to highlight the element
+    //to show the current section that a user is at
+    navTags.forEach((li) => {
+        //Sets the default class properties to be "nonactive"
+        li.classList.remove("active");
+        li.classList.add("nonactive");
+        //Check if the segment ID name is within the classlist
+        //meaning that it should be set as "active"
+        if (li.classList.contains(closestSegment)) {
+            //Sets the matching navigation list item as active
+            li.classList.add("active");
+            li.classList.remove("nonactive");
+        }
+    });
+};
