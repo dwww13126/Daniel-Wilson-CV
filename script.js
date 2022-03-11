790622b0f43af116a148f57779e436914557e3e0
Daniel Wilson <dww6@students.waikato.ac.nz>
2022-03-11 23:01:00 +1300

    Setup extra animations through adding more to the transition JS method

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //Gets the elements which are used for making the initial transition animations
 let infoGrid = document.getElementById("info-grid");
 let infoGridChildren = document.getElementById("info-grid").getElementsByTagName("div");
+let bioTextbox = document.getElementById("bio-textbox");
+let bioTextboxChildren = document.getElementById("bio-textbox").getElementsByTagName("p");
+let awardsTextbox = document.getElementById("awards-textbox");
+let awardsTextboxChildren = document.getElementById("awards-textbox").getElementsByTagName("p");
+let codingContainer = document.getElementById("coding-container");
+let codingContainerChildren = document.getElementById("coding-container").getElementsByTagName("div");
 
 setTimeout(() => { transition() },100);
 
@@ -14,5 +20,24 @@ function transition() {
         setTimeout(() => { infoSec.classList.add("fade-in-1");}, time);
         //Increments time to space out the transitions 
         time = time + 60;
+    }
+    time = 100;
+    setTimeout(() => { bioTextbox.classList.add("fade-in-1");}, time);
+    //Does the same for the other sets
+    for (let bioText of bioTextboxChildren) {
+        setTimeout(() => { bioText.classList.add("fade-in-1");}, time);
+        time = time + 60;
+    }
+    time = 100;
+    setTimeout(() => { awardsTextbox.classList.add("fade-in-1");}, time);
+    for (let awardsText of awardsTextboxChildren) {
+        setTimeout(() => { awardsText.classList.add("fade-in-1");}, time);
+        time = time + 60;
+    }
+    time = 100;
+    setTimeout(() => { codingContainer.classList.add("fade-in-1");}, time);
+    for (let coding of codingContainerChildren) {
+        setTimeout(() => { coding.classList.add("fade-in-1");}, time);
+        time = time + 100;
     }
 }
