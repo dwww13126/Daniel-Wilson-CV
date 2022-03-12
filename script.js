ce7e99e760404e4e11c35a49bdad309bcef791b5
Daniel Wilson <dww6@students.waikato.ac.nz>
2022-03-12 16:23:05 +1300

    Cleaned up content of style.css

--- script.js
+++ script.js
@@ -14,30 +14,30 @@ function transition() {
     //Sets the starting time as 0.1 seconds
     var time = 100;
     //Sets the infoGrid to fade in first
-    setTimeout(() => { infoGrid.classList.add("fade-in-1");}, time);
+    setTimeout(() => { infoGrid.classList.add("fade-in");}, time);
     //Iterates through each of the infoGrid's children
     for (let infoSec of infoGridChildren) {
-        setTimeout(() => { infoSec.classList.add("fade-in-1");}, time);
+        setTimeout(() => { infoSec.classList.add("fade-in");}, time);
         //Increments time to space out the transitions 
         time = time + 60;
     }
     time = 100;
-    setTimeout(() => { bioTextbox.classList.add("fade-in-1");}, time);
+    setTimeout(() => { bioTextbox.classList.add("fade-in");}, time);
     //Does the same for the other sets
     for (let bioText of bioTextboxChildren) {
-        setTimeout(() => { bioText.classList.add("fade-in-1");}, time);
+        setTimeout(() => { bioText.classList.add("fade-in");}, time);
         time = time + 60;
     }
     time = 100;
-    setTimeout(() => { awardsTextbox.classList.add("fade-in-1");}, time);
+    setTimeout(() => { awardsTextbox.classList.add("fade-in");}, time);
     for (let awardsText of awardsTextboxChildren) {
-        setTimeout(() => { awardsText.classList.add("fade-in-1");}, time);
+        setTimeout(() => { awardsText.classList.add("fade-in");}, time);
         time = time + 60;
     }
     time = 100;
-    setTimeout(() => { codingContainer.classList.add("fade-in-1");}, time);
+    setTimeout(() => { codingContainer.classList.add("fade-in");}, time);
     for (let coding of codingContainerChildren) {
-        setTimeout(() => { coding.classList.add("fade-in-1");}, time);
+        setTimeout(() => { coding.classList.add("fade-in");}, time);
         time = time + 100;
     }
 }
